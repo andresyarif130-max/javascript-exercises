756a3d01da96d7863ac5c973c54cadf3721d928d
Andre Syarif <andresyarif130@gmail.com>
2026-05-25 10:19:23 +0800

    make loop for repeat the sum

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function (word, sum) {
   if (num < 0) return 'Error';
   const string = '';
+
+  for (let i = 0; i < sum; i++) {
+    string += word;
+  }
 };
 // Do not edit below this line
 module.exports = repeatString;
