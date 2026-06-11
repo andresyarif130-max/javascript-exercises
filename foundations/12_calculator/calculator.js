9e66eba50f2b08de611c05edd4413973b6058514
Andre Syarif <andresyarif130@gmail.com>
2026-06-11 10:23:21 +0800

    add function

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -10,11 +10,21 @@ const sum = function (array) {
   return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function () {};
+const multiply = function (array) {
+  return array.reduce((sum, current) => sum * current, 1);
+};
 
-const power = function () {};
+const power = function (a, b) {
+  return a ** b;
+};
 
-const factorial = function () {};
+const factorial = function (number) {
+  let result = 1;
+  for (let i = number; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+};
 
 // Do not edit below this line
 module.exports = {
